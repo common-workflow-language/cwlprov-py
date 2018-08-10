987392413473932c6194c9212ee7af6ce84a49cd
Stian Soiland-Reyes <stain@apache.org>
2018-08-10 15:05:34 +0100

    cwlprov-py

---
+++ README.md
@@ -0,0 +1,43 @@
+# CWLProv Python tool
+
+
+
+## License
+
+This repository is distributed under [Apache License, version 2.0](https://www.apache.org/licenses/LICENSE-2.0).
+
+See the file [LICENSE.txt](LICENSE.txt) for details, and [NOTICE](NOTICE) for required notices.
+
+
+## Contributing
+
+cwlprov.py is maintained at https://github.com/common-workflow-language/cwlprov-py/
+
+Feel free to raise an
+[issue](https://github.com/common-workflow-language/cwlprov-py/issues) or a
+[pull request](https://github.com/common-workflow-language/cwlprov-py/pulls) to
+contribute to CWLProv. Contributions are assumed to be covered by 
+[section 5 of the Apache License](https://www.apache.org/licenses/LICENSE-2.0#contributions).
+
+For an informal CWLProv discussion with other developers, join the (relatively
+quiet) Gitter room
+[common-workflow-language/cwlprov](https://gitter.im/common-workflow-language/cwlprov),
+or the (more busy)
+[common-workflow-language/common-workflow-language](https://gitter.im/common-workflow-language/common-workflow-language).
+
+
+### Code of Conduct
+
+The CWL Project is dedicated to providing a harassment-free experience for
+everyone, regardless of gender, gender identity and expression, sexual
+orientation, disability, physical appearance, body size, age, race, or
+religion. We do not tolerate harassment of participants in any form. This code
+of conduct applies to all CWL Project spaces, including the Google Group, the
+Gitter chat room, the Google Hangouts chats, both online and off. Anyone who
+violates this code of conduct may be sanctioned or expelled from these spaces
+at the discretion of the leadership team.
+
+For more details, see our 
+[Code of Conduct](https://github.com/common-workflow-language/common-workflow-language/blob/master/CODE_OF_CONDUCT.md).
+
+
---
+++ setup.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+
+## © Copyright 2018 Software Freedom Conservancy (SFC)
+##
+## Licensed under the Apache License, Version 2.0 (the "License");
+## you may not use this file except in compliance with the License.
+## You may obtain a copy of the License at
+##
+##     http://www.apache.org/licenses/LICENSE-2.0
+##
+## Unless required by applicable law or agreed to in writing, software
+## distributed under the License is distributed on an "AS IS" BASIS,
+## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+## See the License for the specific language governing permissions and
+## limitations under the License.
+
+__author__      = "Stian Soiland-Reyes <https://orcid.org/0000-0001-9842-9718>"
+__copyright__   = "© 2018 Software Freedom Conservancy (SFC)"
+__license__     = "Apache License, version 2.0 (https://www.apache.org/licenses/LICENSE-2.0)"
+
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+#with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+#  long_description = f.read()
+
+long_description = """
+cwlprov API for Python
+"""
+
+
+setup(
+  name = 'cwlprov',
+  packages = find_packages(exclude=['contrib', 'docs', 'tests']), # Required
+  version = '0.0.1',
+  description = 'cwlprov API for Python',
+  long_description=long_description,
+  author = 'Stian Soiland-Reyes',
+  author_email = 'stain@apache.org',
+  
+  # https://www.apache.org/licenses/LICENSE-2.0
+  license = "Apache License, Version 2.0",
+  url = 'https://github.com/common-workflow-language/cwlprov-py',
+#  download_url = 'https://github.com/stain/arcp-py/archive/0.1.0.tar.gz',
+  keywords = "cwl prov cwlprov provenance",
+  
+  install_requires=[
+          'prov >= 1.5.1',
+          'bagit >= 1.6.4',
+          'arcp >= 0.2.0',
+          'rdflib-jsonld >= 0.4.0',
+          'rdflib >= 4.2.2',        
+  ],
+  python_requires='>=3.6, <4',
+  classifiers=[
+    # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    'Development Status :: 2 - Pre-Alpha',
+
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+     # 'License :: OSI Approved :: Apache Software License',
+     # https://github.com/pypa/pypi-legacy/issues/564
+    #'License :: OSI Approved',
+    # 'License :: OSI Approved :: Apache License, Version 2.0 (Apache-2.0)',  
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.6',
+    'Topic :: Internet',
+    'Topic :: System :: Archiving :: Packaging',
+],
+  
+)
