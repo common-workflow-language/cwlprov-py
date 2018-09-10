80a9e6821e36b015559855c975cc099b9f8b90ce
Stian Soiland-Reyes <stain@apache.org>
2018-09-10 16:17:47 +0100

    refactor validation

--- cwlprov/tool.py
+++ cwlprov/tool.py
@@ -30,26 +30,27 @@ import pathlib
 
 from enum import IntEnum
 
-BAGIT_RO_PROFILES = set((
+BAGIT_RO_PROFILES = (
     "https://w3id.org/ro/bagit/profile", 
     "http://raw.githubusercontent.com/fair-research/bdbag/master/profiles/bdbag-ro-profile.json"
-))
-CWLPROV_SUPPORTED = set((
+)
+CWLPROV_SUPPORTED = (
     # Decreasing order as first item is output as example
     "https://w3id.org/cwl/prov/0.5.0",
     "https://w3id.org/cwl/prov/0.4.0",
     "https://w3id.org/cwl/prov/0.3.0",
-))
+)
 
 MANIFEST_JSON = posixpath.join("metadata", "manifest.json")
 
 class Status(IntEnum):
     """Exit codes from main()"""
     OK = 0
-    BAG_NOT_FOUND = 1
-    INVALID_BAG = 2
-    MISSING_PROFILE = 3
-    UNSUPPORTED_CWLPROV_VERSION = 4
+    UNKNOWN_COMMAND = 1
+    BAG_NOT_FOUND = 2
+    INVALID_BAG = 3
+    MISSING_PROFILE = 4
+    UNSUPPORTED_CWLPROV_VERSION = 5
 
 def parse_args(args=None):
     parser = argparse.ArgumentParser(description='cwlprov')
@@ -58,9 +59,10 @@ def parse_args(args=None):
         default=None
         )
     subparsers = parser.add_subparsers(title='commands', dest="cmd")
-    parser_info = subparsers.add_parser('info', help='display brief info about the CWLProv RO')
     parser_validate = subparsers.add_parser('validate', help='validate the CWLProv RO')
-    parser_inputs = subparsers.add_parser('inputs', help='show inputs')
+    parser_info = subparsers.add_parser('info', help='CWLProv RO')
+    parser_runs = subparsers.add_parser('runs', help='show workflow runs')
+    parser_who = subparsers.add_parser('who', help='who ran the workflow')
 
     return parser.parse_args(args)
 
@@ -76,6 +78,69 @@ def _determine_bagit_folder(folder=None):
             return None
         folder = folder.parent
 
+def _info_set(bag, key):
+    v = bag.info.get(key, [])
+    if isinstance(v, list):
+        return set(v)
+    else:
+        return set([v])
+
+def validate_bag(bag, full_validation=False):
+    valid_bag = bag.validate(fast=not full_validation)
+    if not valid_bag:
+        print("Invalid BagIt folder: %s" % bag.path,
+            file=sys.stderr)
+        # Specific errors already output from bagit library
+        return Status.INVALID_BAG
+    # Check we follow right profile
+    profiles = _info_set(bag, "BagIt-Profile-Identifier")
+    supported_ro = set(BAGIT_RO_PROFILES).intersection(profiles)
+    if not supported_ro:
+        print("Missing BdBag profile: %s" % bag.path,
+            file=sys.stderr)
+        if full_validation:
+            print("Try adding to %s/bag-info.txt:" % bag.path)
+            print("BagIt-Profile-Identifier: %s" % BAGIT_RO_PROFILES[0])
+            return Status.MISSING_PROFILE
+    # Check we have a manifest
+    has_manifest = MANIFEST_JSON in bag.tagfile_entries()
+    if not has_manifest:
+        print("Missing from tagmanifest: " + MANIFEST_JSON)
+        return Status.MISSING_MANIFEST
+    return Status.OK
+
+def validate_ro(ro, full_validation=False):
+    # If it has this prefix, it's probably OK
+    cwlprov = set(p for p in ro.conformsTo if p.startswith("https://w3id.org/cwl/prov/"))
+    if not cwlprov:
+        print("Missing CWLProv profile: %s" % ro.bag.path,
+            file=sys.stderr)
+        if full_validation:
+            print("Try adding to %s/metadata/manifest.json:" % ro.bag.path)
+            print('{\n  "id": "/",\n  "conformsTo", "%s",\n  ...\n}' %
+                CWLPROV_SUPPORTED[0])
+            return Status.MISSING_PROFILE
+    supported_cwlprov = set(CWLPROV_SUPPORTED).intersection(cwlprov)
+    if cwlprov and not supported_cwlprov:
+        # Probably a newer one this code don't support yet; it will 
+        # probably be fine
+        print("Unsupported CWLProv version: %s" % cwlprov, file=sys.stderr)
+        if full_validation:
+            print("Supported profiles:\n %s" %
+                    "\n ".join(CWLPROV_SUPPORTED)
+                 )
+            return Status.UNSUPPORTED_CWLPROV_VERSION
+    return Status.OK
+
+def info(ro, args):
+    return Status.OK
+
+def who(ro, args):
+    return Status.OK
+
+def runs(ro, args):
+    return Status.OK
+
 def main(args=None):
     # type: (...) -> None
     """cwlprov command line tool"""
@@ -85,64 +150,38 @@ def main(args=None):
     if not folder:        
         print("Could not find bagit.txt, try cwlprov -d mybag/", file=sys.stderr)
         return Status.BAG_NOT_FOUND
+    
+    full_validation = args.cmd == "validate"
 
     ## BagIt check
-    bag = BDBag(folder)
-    # Always do a minimal validation, but
-    # will also test checksum on --validate
-    validate = args.cmd == "validate"
-    valid_bag = bag.validate(fast=validate)
-
-    ## RO check
-    profiles = set(bag.info.get("BagIt-Profile-Identifier", ()))
-    supported_ro = BAGIT_RO_PROFILES.intersection(profiles)
+    bag = BDBag(str(folder))
+    invalid = validate_bag(bag, full_validation)
+    if invalid:
+        return invalid
     
-    has_manifest = MANIFEST_JSON in bag.tagfile_entries()
-    if not has_manifest:
-        print("Missing " + MANIFEST_JSON)
-        return Status.MISSING_MANIFEST
-
     ro = ResearchObject(bag)
-    cwlprov = set()
-    for p in ro.conformsTo:
-        if "https://w3id.org/cwl/prov/" in p:
-            cwlprov.add(p)
-    supported_cwlprov = CWLPROV_SUPPORTED.intersection(cwlprov)
-    if cwlprov and not supported_cwlprov:
-        print("Unsupported CWLProv version: %s" % cwlprov, file=sys.stderr)
+    invalid = validate_ro(ro, full_validation)
+    if invalid:
+        return invalid
 
+    if full_validation:
+        print("Valid: %s" % folder)
+        return Status.OK
 
-    ##PROV check
-    # TODO: Prov check
-
-    if validate:
-        # TODO: More checks, e.g. every PROV file valid?
-        if not valid_bag:
-            print("Invalid BagIt folder: %s" % folder,
-                file=sys.stderr)
-            return Status.INVALID_BAG
-        if not supported_ro:
-            print("Missing BdBag profile (e.g. %s): %s" %
-                (next(iter(BAGIT_RO_PROFILES)), folder),
-                file=sys.stderr)
-            return Status.MISSING_PROFILE
-        if not cwlprov:
-            print("Missing CWLProv profile (e.g. %s): %s" %
-                (next(iter(CWLPROV_SUPPORTED)), folder),
-                file=sys.stderr)
-            return Status.MISSING_PROFILE
-        if not supported_cwlprov:
-            # Warning already printed above
-            print("Supported CWLProv profiles: %s" %
-                " ".join(CWLPROV_SUPPORTED),
-                file=sys.stderr)
-            return Status.MISSING_PROFILE
-
-
-            return Status.UNSUPPORTED_CWLPROV_VERSION
-
-    # Probably went fine if we made it to here
-    return Status.OK
+    # Else, find the other commands
+    COMMANDS = {
+        "info": info,
+        "runs": runs,
+        "who": who
+    }
+    
+    cmd = COMMANDS.get(args.cmd)
+    if not cmd:
+        # Light-weight validation
+        print("Detected CWLProv research Object: %s" % folder)
+        return Status.OK
+    
+    return cmd(ro, args)
 
 if __name__ == "__main__":
     sys.exit(main())
