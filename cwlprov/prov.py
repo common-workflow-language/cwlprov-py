b12ec31ddf067aca9afbed061bb2f12db1420443
Stian Soiland-Reyes <stain@apache.org>
2018-09-13 11:27:38 +0100

    Provenance module

---
+++ cwlprov/prov.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+  
+## © 2018 Software Freedom Conservancy (SFC)
+##
+## Licensed under the Apache License, Version 2.0 (the "License");
+## you may not use this file except in compliance with the License.
+## You may obtain a copy of the License at
+##
+##     http://www.apache.org/licenses/LICENSE-2.0
+##
+## Unless required by applicable law or agreed to in writing, software
+## distributed under the License is distributed on an "AS IS" BASIS,
+## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+## See the License for the specific language governing permissions and
+## limitations under the License.
+
+"""
+cwlprov Provenance
+
+"""
+__author__      = "Stian Soiland-Reyes <https://orcid.org/0000-0001-9842-9718>"
+__copyright__   = "© 2018 Software Freedom Conservancy (SFC)"
+__license__     = "Apache License, version 2.0 (https://www.apache.org/licenses/LICENSE-2.0)"
+
+class Provenance:
+    def __init__(self, ro, run=None):
+        self.ro = ro
+        self.run = None
+        
