e85c24c8bc4a437c3cac89e0f23e63ddfb7ebd6e
Michael R. Crusoe <michael.crusoe@gmail.com>
2025-09-20 13:16:51 +0200

    new verson 0.2.0

--- cwlprov/__init__.py
+++ cwlprov/__init__.py
@@ -20,4 +20,4 @@ __copyright__ = "© 2018 Software Freedom Conservancy (SFC)"
 __license__ = (
     "Apache License, version 2.0 (https://www.apache.org/licenses/LICENSE-2.0)"
 )
-__version__ = "0.1.2"
+__version__ = "0.2.0"
